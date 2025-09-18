05dc4e60f4314b25e63b7f8a190b9a2f31b46ab4
Vaibhav <vaibhavpunyani772@gmail.com>
2025-09-18 12:56:06 +0530

    last commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="z-10 max-w-screen-xl px-4 pb-10 sm:pb-20 mx-auto sm:px-6 lg:px-8">
           <div className="flex flex-col sm:flex-row items-center sm:items-center sm:justify-between gap-8">
             {/* Text Content */}
-            <div className="space-y-6 text-center sm:text-left sm:max-w-lg sm:mt-0 mt-20">
+            <div className="space-y-6 text-center sm:text-left sm:max-w-lg sm:mt-0 mt-25">
               <h2 className="text-3xl sm:text-5xl font-bold">
                 Download Now
                 <span className="block text-2xl sm:text-4xl text-white">
